e1228d121f1a65cd64766ab7048c8e1bced4d75c
fahim25 <fahimmuntasir201@gmail.com>
2023-03-12 23:05:08 +0600

    setUp

---
+++ src/main/java/com/ppmtool/model/Project.java
@@ -0,0 +1,100 @@
+package com.ppmtool.model;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String projectDesc;
+    private Date startDate;
+    private Date endDate;
+
+    public Project() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectDesc() {
+        return projectDesc;
+    }
+
+    public void setProjectDesc(String projectDesc) {
+        this.projectDesc = projectDesc;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCratedDate() {
+        return cratedDate;
+    }
+
+    public void setCratedDate(Date cratedDate) {
+        this.cratedDate = cratedDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    private Date cratedDate;
+    private Date updatedDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.cratedDate = new Date();
+    }
+
+    @PreUpdate
+    protected  void onUpdate(){
+        this.updatedDate = new Date();
+    }
+
+
+}
---
+++ src/main/java/com/ppmtool/services/ProjectService.java
@@ -0,0 +1,22 @@
+package com.ppmtool.services;
+
+import com.ppmtool.model.Project;
+import com.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+
+}
