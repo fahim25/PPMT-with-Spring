48af22520f3e53afaa2a93985eee76f1d70b5017
fahim25 <fahimmuntasir201@gmail.com>
2023-03-14 00:33:00 +0600

    handle validation

--- src/main/java/com/ppmtool/controller/ProjectController.java
+++ src/main/java/com/ppmtool/controller/ProjectController.java
@@ -1,14 +1,18 @@
 package com.ppmtool.controller;
 
 import com.ppmtool.model.Project;
+import com.ppmtool.services.MapValidationService;
 import com.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,11 +21,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationService validationService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = validationService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
-
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 
