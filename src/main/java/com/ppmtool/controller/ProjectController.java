0a964876ce7a771b1c08a9b73675d516009d5139
fahim25 <fahimmuntasir201@gmail.com>
2023-03-24 12:01:20 +0600

    Update ProjectController.java

--- src/main/java/com/ppmtool/controller/ProjectController.java
+++ src/main/java/com/ppmtool/controller/ProjectController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class ProjectController {
 
     @Autowired
