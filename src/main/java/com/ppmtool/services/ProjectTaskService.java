740e57d742d0dcf4ef819030ee2bdb7039bfc7f6
fahim25 <fahimmuntasir201@gmail.com>
2023-08-18 12:46:54 +0600

    Find ProjectTask by projectSequence Validation

--- src/main/java/com/ppmtool/services/ProjectTaskService.java
+++ src/main/java/com/ppmtool/services/ProjectTaskService.java
@@ -84,9 +84,26 @@ public class ProjectTaskService {
     public  ProjectTask findPTByProjectSequence(String backlog_id, String pt_id){
 
         /*make sure we are searching the on the right backlog*/
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+
+        if (backlog == null){
+            throw new ProjectNotFoundException("Project with ID: '" +backlog_id+"' does not exist");
+        }
+
+        /* make sure that out task exist */
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        if (projectTask == null){
+            throw new ProjectNotFoundException("Project with ID: '" +backlog_id+"' does not exist");
+        }
+        /* mack sure the backlog/project id in the path corresponding to the right project */
+
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project task '" +pt_id+"' does not exist in project '" +backlog_id + "' " );
+        }
 
 
-        return projectTaskRepository.findByProjectSequence(backlog_id);
+        return projectTask;
     }
 
 
